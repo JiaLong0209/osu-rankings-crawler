e94d849089fd4f2c5c405e75f4336f85074b312b
JiaLong0209 <smt02091395@gmail.com>
2024-05-31 22:11:29 +0800

    240601 Add functions.py

--- osu_crawler/utility/functions.py
+++ osu_crawler/utility/functions.py
@@ -1,9 +1,10 @@
+import re
 
-def replace_space(str):
-    return str.replace(" ", "").replace("\n", "")
+def str_preprocess(s):
+    return re.sub(r'[#\s]+', '', s)
 
-def replace_space_in_list(p_list):
-    return list(map(replace_space, p_list))
+def str_preprocess_in_list(p_list):
+    return list(map(str_preprocess, p_list))
 
 def print_dict(p_dict):
     print('\n-------print_dict()-----------')
